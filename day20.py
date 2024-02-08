02efcd68db727453848eb40556b1e8763dba6f00
normal <specialnormal@naver.com>
2024-02-08 13:49:55 +0900

    day01 v0.7 guess number

--- day20.py
+++ day20.py
@@ -1,8 +1,17 @@
-import mymath
-import time
+import random
 
-if __name__ == "__main__":
-    n = int(input("input n : "))
-    r = int(input("input r : "))
+note = random.randint(1, 100)
+count = 1
+while True:
+    n = int(input("숫자를 입력하세요."))
+    if note < n:
+        print("틀렸습니다! down")
+    elif note > n:
+        print("틀렸습니다! up")
+    elif note == n:
+        print(f"정답입니다. 총 {count}회 만에 맞추셨습니다.")
+        break
+    else:
+        print("잘못된 값을 입력했습니다. 다시!!")
 
-    print(f"{mymath.combination(n,r, mymath.factorial_func)}")
+    count += 1
