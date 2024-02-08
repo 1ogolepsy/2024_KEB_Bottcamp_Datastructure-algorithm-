cb892fe12f7b65f6b9785ae70cfcdf598fd298db
normal <specialnormal@naver.com>
2024-02-08 17:40:46 +0900

    day01 v1.0 node delete, find

--- day20.py
+++ day20.py
@@ -39,19 +39,53 @@ def insert_node(find_data, insert_data):
     node.data = insert_data
     current.link = node
 
+def delete_node(delete_data):
+    global head, current, pre
+
+    if head.data == delete_data:         # 첫 번째 노드 삭제
+        current = head
+        head = head.link
+        del(current)
+        print("첫 번째 노드가 삭제됨")
+        return
+
+    current = head
+    while current.link is not None:
+        pre = current
+        current = current.link
+        if current.data == delete_data:
+            pre.link = current.link
+            del(current)
+            print("중간 노드가 삭제됨")
+            return
+
+    print(f"{delete_data}은/는 삭제할 수 없음")
+
+def find_node(find_data) :
+    global head, current, pre
+
+    current = head
+    if current.data == find_data:
+        return current
+    while current.link is not None:
+        current = current.link
+        if current.data == find_data:
+            return current
+    return Node()
+
 
 memory = []
 head, current, pre = None, None, None
-dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
+data_array = ["다현", "정연", "쯔위", "사나", "지효"]
 
 if __name__ == "__main__":
 
     node = Node()
-    node.data = dataArray[0]
+    node.data = data_array[0]
     head = node
     memory.append(node)
 
-    for data in dataArray[1:]:
+    for data in data_array[1:]:
         pre = node
         node = Node()
         node.data = data
@@ -59,11 +93,6 @@ if __name__ == "__main__":
         memory.append(node)
 
     print_nodes(head)
-
-    insert_node("다현", "화사")
-    print_nodes(head)
-    insert_node("사나", "솔라")
-    print_nodes(head)
-    insert_node("동균", "문별")
-    print_nodes(head)
-
+    print(find_node("다현").data)
+    print(find_node("재남").data)
+    print(find_node("사나").data)
