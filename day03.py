52f2127d09df114de84cb1f8b876a690ddb6682d
normal <specialnormal@naver.com>
2024-02-13 17:29:30 +0900

    day02 v1.8 bianary tree

--- day03.py
+++ day03.py
@@ -1,65 +1,69 @@
-## 함수 선언 부분 ##
-def is_queue_full():
-    global size, queue, front, rear
-    if (rear+1) % size == front:
-        return True
-    else:
-        return False
-def is_queue_empty():
-    global size, queue, front, rear
-    if front == rear:
-        return True
-    else :
-        return False
+class Treenode() :
+    def __init__ (self) :
+        self.left = None
+        self.data = None
+        self.right = None
 
-def en_queue(data):
-    global size, queue, front, rear
-    if is_queue_full():
-        print("큐가 꽉 찼습니다.")
+node1 = Treenode()
+node1.data = '화사'
+
+node2 = Treenode()
+node2.data = '솔라'
+node1.left = node2
+
+node3 = Treenode()
+node3.data = '문별'
+node1.right = node3
+
+node4 = Treenode()
+node4.data = '휘인'
+node2.left = node4
+
+node5 = Treenode()
+node5.data = '쯔위'
+node2.right = node5
+
+node6 = Treenode()
+node6.data = '선미'
+node3.left = node6
+
+node9 = Treenode()
+node9.data = '다현'
+node4.right = node9
+
+node13 = Treenode()
+node13.data = '사나'
+node6.right = node13
+
+def preorder(node) :
+    if node == None:
         return
-    rear = (rear + 1) % size
-    queue[rear] = data
+    print(node.data, end='->')
+    preorder(node.left)
+    preorder(node.right)
 
-def de_queue():
-    global size, queue, front, rear
-    if is_queue_empty():
-        print("큐가 비었습니다.")
-        return None
-    front = (front + 1) % size
-    data = queue[front]
-    queue[front] = None
-    return data
+def inorder(node):
+    if node == None :
+        return
+    inorder(node.left)
+    print(node.data, end='->')
+    inorder(node.right)
 
-def peek():
-    global size, queue, front, rear
-    if is_queue_empty():
-        print("큐가 비었습니다.")
-        return None
-    return queue[(front+1) % size]
+def postorder(node):
+    if node == None :
+        return
+    postorder(node.left)
+    postorder(node.right)
+    print(node.data, end='->')
 
-size = int(input("큐의 크기를 입력하세요 ==> "))
-queue = [None for _ in range(size)]
-front = rear = 0
+print('전위 순회 : ', end = '')
+preorder(node1)
+print('끝')
 
-if __name__ == "__main__":
-    menu = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+print('중위 순회 : ', end = '')
+inorder(node1)
+print('끝')
 
-    while (menu != 'X' and menu != 'x'):
-        if menu== 'I' or menu == 'i':
-            data = input("입력할 데이터 ==> ")
-            en_queue(data)
-            print("큐 상태 : ", queue)
-        elif menu== 'E' or menu == 'e':
-            data = de_queue()
-            print("추출된 데이터 ==> ", data)
-            print("큐 상태 : ", queue)
-        elif menu== 'V' or menu == 'v':
-            data = peek()
-            print("확인된 데이터 ==> ", data)
-            print("큐 상태 : ", queue)
-        else :
-            print("입력이 잘못됨")
-
-        menu = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
-
-    print("프로그램 종료!")
+print('후위 순회 : ', end = '')
+postorder(node1)
+print('끝')
